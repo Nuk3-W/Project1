9b22d9bf7affca3c99645f5cb8400c1373b3c6ac
Khalifa Alsuwaidi <100087548+K-Alsuwaidi@users.noreply.github.com>
2024-10-04 16:08:35 -0400

    Update Parta.py

--- Parta.py
+++ Parta.py
@@ -12,14 +12,14 @@ for i in data_reader:
         i[0] = "101"
         x+=1
     if i[0] not in UHF_to_measurements.keys():
-        UHF_to_measurements[i[0]] = [i[-1]]
+        UHF_to_measurements[i[0]] = [(i[1:])]
         
     else:
-        UHF_to_measurements[i[0]].append(i[-1])
+        UHF_to_measurements[i[0]].append((i[1:]))
         
     if i[2] not in date_to_measurements.keys():
-        date_to_measurements[i[2]] = [i[-1]]
+        date_to_measurements[i[2]] = [(i[1:])]
     else:
-        date_to_measurements[i[2]].append(i[-1])
+        date_to_measurements[i[2]].append((i[1:]))
 
 
